a4fa360cc67dad436c4e2f6f34ecce63040b7d69
propj <66355959+propj@users.noreply.github.com>
2024-05-23 18:23:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
 let conversation = document.getElementById('conversation');
 let userInput = document.getElementById('userInput');
 
+let userProfile = {
+    name: '',
+    hobbies: [],
+    favoriteMusic: '',
+    favoriteArtist: '',
+    pets: '',
+    favoriteFood: '',
+    vacationDestination: '',
+    favoriteBookGenre: '',
+    favoriteMovie: '',
+    sports: '',
+    otherInterests: []
+};
+
+const responses = [
+    { keywords: ['name'], response: (userInput) => `Nice to meet you, ${userInput.split(' ').pop()}! What's your favorite hobby?` },
+    { keywords: ['hobby', 'hobbies'], response: (userInput) => `That sounds fun! What's your favorite type of music?` },
+    { keywords: ['music'], response: (userInput) => `I love music too! Who's your favorite artist?` },
+    { keywords: ['artist'], response: (userInput) => `Great choice! Do you have any pets?` },
+    { keywords: ['pets'], response: (userInput) => `Pets are wonderful! What's your favorite food?` },
+    { keywords: ['food'], response: (userInput) => `Yum! I love that too. What's your dream vacation destination?` },
+    { keywords: ['vacation', 'destination'], response: (userInput) => `That sounds amazing! Do you enjoy reading books?` },
+    { keywords: ['books', 'reading'], response: (userInput) => `Books are a great escape! What's your favorite genre?` },
+    { keywords: ['genre'], response: (userInput) => `Nice! Have you watched any good movies lately?` },
+    { keywords: ['movies', 'movie'], response: (userInput) => `Movies are fun! What's your favorite movie of all time?` },
+    { keywords: ['favorite'], response: (userInput) => `That's a classic! Do you play any sports?` },
+    { keywords: ['sports'], response: (userInput) => `Sports are great for staying active! Do you have any other interests?` },
+    { keywords: ['interests', 'interest'], response: (userInput) => `Tell me more about it!` }
+];
+
+const defaultResponses = [
+    "That's interesting! Tell me more!",
+    "Really? I'd love to hear more about that.",
+    "Oh wow! What else can you tell me?",
+    "That's amazing! Can you elaborate?",
+    "I see. Do you have any other hobbies or interests?"
+];
+
 function sendMessage() {
     let userMessage = userInput.value;
     if (userMessage.trim() !== "") {
@@ -18,13 +56,51 @@ function appendMessage(sender, message) {
 }
 
 function getResponse(message) {
-    let response = "";
-    if (message.toLowerCase().includes('name')) {
-        response = "Nice to meet you! What's your favorite hobby?";
-    } else if (message.toLowerCase().includes('hobby')) {
-        response = "That sounds fun! What's your favorite type of music?";
-    } else {
-        response = "That's interesting! Tell me more!";
-    }
+    let response = generateResponse(message.toLowerCase());
     setTimeout(() => appendMessage('Girlfriend', response), 1000);
+}
+
+function generateResponse(message) {
+    for (let i = 0; i < responses.length; i++) {
+        for (let keyword of responses[i].keywords) {
+            if (message.includes(keyword)) {
+                updateUserProfile(keyword, message);
+                return responses[i].response(message);
+            }
+        }
+    }
+    return defaultResponses[Math.floor(Math.random() * defaultResponses.length)];
+}
+
+function updateUserProfile(keyword, message) {
+    if (keyword === 'name') {
+        userProfile.name = message.split(' ').pop();
+    } else if (keyword === 'hobby' || keyword === 'hobbies') {
+        userProfile.hobbies.push(message);
+    } else if (keyword === 'music') {
+        userProfile.favoriteMusic = message;
+    } else if (keyword === 'artist') {
+        userProfile.favoriteArtist = message;
+    } else if (keyword === 'pets') {
+        userProfile.pets = message;
+    } else if (keyword === 'food') {
+        userProfile.favoriteFood = message;
+    } else if (keyword === 'vacation' || keyword === 'destination') {
+        userProfile.vacationDestination = message;
+    } else if (keyword === 'books' || keyword === 'reading') {
+        userProfile.favoriteBookGenre = message;
+    } else if (keyword === 'movies' || keyword === 'movie') {
+        userProfile.favoriteMovie = message;
+    } else if (keyword === 'sports') {
+        userProfile.sports = message;
+    } else if (keyword === 'interests' || keyword === 'interest') {
+        userProfile.otherInterests.push(message);
+    }
+}
+
+function personalizedResponse(message) {
+    if (userProfile.name) {
+        return `By the way, ${userProfile.name}, ${message}`;
+    }
+    return message;
 }
